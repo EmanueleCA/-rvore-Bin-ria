a8e86904a20fbd85dd9fa7b9a1339ffb01f4a2cd
Emanuele Araújo <emanuelecaraujo21@gmail.com>
2025-08-05 15:51:58 -0300

    montando a base de uma árvore binária

---
+++ BinaryTree.java
@@ -0,0 +1,38 @@
+public class BinaryTree {
+    private int value;
+
+    private BinaryTree leftChild;
+    private BinaryTree rightChild;
+
+    public BinaryTree(int value) {
+        this.value = value;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public BinaryTree getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(BinaryTree leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public BinaryTree getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(BinaryTree rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    
+}
