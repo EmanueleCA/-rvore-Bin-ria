db11f2bad460deaea8fdf10545e6a0fdc29f3f13
Emanuele Araújo <emanuelecaraujo21@gmail.com>
2025-08-25 16:26:52 -0300

    método inOrder

--- BinaryTree.java
+++ BinaryTree.java
@@ -17,11 +17,38 @@ public class BinaryTree {
    public BinaryTree(int info){
     root = new Node(info);
    }
+   public void insert(int info){
+    insert(root, info);
+   }
    //método insert
-   public void insert(Node root, int info){
-    
+   private void insert(Node root, int info){
+    if(info <= root.info){
+        //incluindo um novo nó à esquerda da raiz
+        if(root.left == null){
+            root.left = new Node(info);
+        }
+        //incluindo um novo nó abaixo da esquerda da raiz
+        else{
+            this.insert(root.left, info);
+        }
+    }else{
+        if(root.right == null){
+            root.right = new Node(info);
+        }else{
+            this.insert(root.right, info);
+        }
+    }
    }
 
+   public void inOrder(Node root){
+        if(root == null){
+            return;
+        }else{
+            inOrder(root.left());
+            System.out.println(root.info());
+            inOrder(root.right());
+        }
+   }
 
 
 
